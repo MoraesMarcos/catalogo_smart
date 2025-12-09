8676ce3c2fccca03c7a262b45db0ef56edaf6324
FlavioWallefy1 <fwso@discente.ifpe.edu.br>
2025-12-09 11:43:29 -0300

    alteração nos arquivos theme

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val GreenPrimary = Color(0xFF006874)
+val GreenSecondary = Color(0xFF4A6267)
+val GreenTertiary = Color(0xFF525E7D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HighlightFavorite = Color(0xFFFF6F52)
+
+val HighlightCategory = Color(0xFF6750A4)
+
+val LightBackground = Color(0xFFEFF4F5)
+
+val LightSurface = Color(0xFFFFFFFF)
+
+val DarkPrimary = Color(0xFF4FD8EB)
+val DarkBackground = Color(0xFF191C1D)
+val DarkSurface = Color(0xFF202324)
